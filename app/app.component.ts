33f56ed3a8e1a4512fe5e832ee1a3529e0cc2603
Russell Wenban <russell@factornine.co.uk>
2016-04-02 19:22:23 +0100

    – Remove repeated title

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,6 @@ import {SelectComponent} from './components/SelectComponent';
     directives: [SelectComponent],
     template: `
     <h1>Simple API</h1>
-    <h1>Simple API</h1>
     <ul>
       <li>Will need an Input to call API</li>
       <li>Will need a Service to call that API</li>
